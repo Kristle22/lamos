1dd2b29bca9e08648705baefc6b40c773e0b0d16
Kristina <crislayn@yahoo.com>
2022-05-10 01:08:30 +0300

    Basic loops: do..while

--- Basic_loops/app.js
+++ Basic_loops/app.js
@@ -42,10 +42,9 @@ console.log('3)---------------------------');
 
 let greeting = 'Labas';
 let i = 0;
-while (i < 5) {
-  i++;
+do {
   console.log(greeting);
-}
+} while (++i < 5);
 
 for (let i = 0; i < 5; i++) {
   console.log(greeting);
@@ -55,24 +54,26 @@ for (let i = 0; i < 5; i++) {
 console.log('4)---------------------------');
 
 let itr = 0;
-while (itr < 7) {
-  itr++;
+while (itr++ < 7) {
   console.log(random);
 }
 
 // 5)
 console.log('5)---------------------------');
 
-for (let i = 0; i < random; i++) {
+// for (let i = 0; i < random; i++) {
+//   console.log(random);
+// }
+let spin = 0;
+do {
   console.log(random);
-}
+} while (++spin < random);
 
 // 6)
 console.log('6)---------------------------');
 
 let j = 0;
-while (j < random && random > 7) {
-  j++;
+while (j++ < random && random > 7) {
   console.log(random);
 }
 
@@ -100,118 +101,137 @@ console.log('Skaiciai ir suma: ', numbAndSum);
 // 8)
 console.log('8) a)---------------------------');
 
-// let randomNum;
-// let iter = 0;
-// while (true) {
-//   iter++;
-//   if (randomNum < 12) {
-//     break;
-//   }
-//   randomNum = rand(10, 25);
-//   console.log(randomNum);
-// }
+let randomNum;
+let loop = 0;
+let sumLess18 = 0;
+let rejectedNum = 0;
+let oddCount = 0;
+let evenCount = 0;
+let loopIter = 0;
+
+do {
+  randomNum = rand(10, 25);
+  console.log(randomNum);
+  loop++;
+  if (randomNum <= 18) {
+    sumLess18 += randomNum;
+  } else {
+    rejectedNum++;
+  }
+  if (randomNum % 2) {
+    oddCount++;
+  } else {
+    evenCount++;
+  }
+  if (randomNum < 12) {
+    loopIter++;
+    console.log(`[${loopIter}] loop iteration is over.`);
+  }
+
+} while (randomNum >= 12 || loop <= 20);
+
+console.log(`Ciklas prasisuko ${loop} kart.`);
+console.log(`Ne didesniu uz 18 skaiciu suma: ${sumLess18}`);
+console.log(`Atmestu reiksmiu kiekis: ${rejectedNum}`);
+console.log(`Cikle yra: ${oddCount} nelyg. ir ${evenCount} lyg. skaiciai`);
+console.log(`Buvo atlikta ${loopIter} ciklo pakartojimu.`);
+
 console.log('8) b)---------------------------');
 
-let randomNumb;
-let count = 0;
-let SumBelow18 = 0;
-let rejected = 0;
-let oddNums = 0;
-let evenNums = 0;
-let loopCount = 0;
+// let randomNumb;
+// let count = 0;
+// let SumBelow18 = 0;
+// let rejected = 0;
+// let oddNums = 0;
+// let evenNums = 0;
+// let loopCount = 0;
 
-for (i = 0; ; i++) {
-  if (randomNumb < 12) {
-    loopCount++;
-    console.log(`[${loopCount}] loopCount is over.`)
-    break;
-  }
-  count++;
-  randomNumb = rand(10, 25);
-  console.log(randomNumb);
+// for (i = 0; ; i++) {
+//   if (randomNumb < 12) {
+//     loopCount++;
+//     console.log(`[${loopCount}] loopCount is over.`)
+//     break;
+//   }
+//   count++;
+//   randomNumb = rand(10, 25);
+//   console.log(randomNumb);
 
-  if (randomNumb < 18) {
-    SumBelow18 += randomNumb;
-  }
-  if (randomNumb > 18) {
-    rejected++;
-  }
-  if (randomNumb % 2) {
-    oddNums++
-  } else {
-    evenNums++;
-  }
-  while (count <= 20) {
-    count++;
-    randomNumb = rand(10, 25);
-    console.log(randomNumb);
-    if (randomNumb < 12) {
-      loopCount++;
-      console.log(`[${loopCount}] loopCount is over.`)
-    }
-  }
-}
-console.log(`Ciklas prasisuko ${count} kart.`);
-console.log(`Ne didesniu uz 18 skaiciu suma: ${SumBelow18}`);
-console.log(`Atmestu reiksmiu kiekis: ${rejected}`);
-console.log(`Cikle yra: ${oddNums} nelyg. ir ${evenNums} lyg. skaiciai`);
-console.log(`Buvo atlikta ${loopCount} ciklo pakartojimu.`);
+//   if (randomNumb <= 18) {
+//     SumBelow18 += randomNumb;
+//   }
+//   if (randomNumb > 18) {
+//     rejected++;
+//   }
+//   if (randomNumb % 2) {
+//     oddNums++
+//   } else {
+//     evenNums++;
+//   }
+//   while (count <= 20) {
+//     count++;
+//     randomNumb = rand(10, 25);
+//     console.log(randomNumb);
+//     if (randomNumb < 12) {
+//       loopCount++;
+//       console.log(`[${loopCount}] loopCount is over.`)
+//     }
+//   }
+// }
+// console.log(`Ciklas prasisuko ${count} kart.`);
+// console.log(`Ne didesniu uz 18 skaiciu suma: ${SumBelow18}`);
+// console.log(`Atmestu reiksmiu kiekis: ${rejected}`);
+// console.log(`Cikle yra: ${oddNums} nelyg. ir ${evenNums} lyg. skaiciai`);
+// console.log(`Buvo atlikta ${loopCount} ciklo pakartojimu.`);
 
 // 9)
 console.log('9)---------------------------');
 
 let randomNumber;
-let iterator = 0;
+let randomInner;
 let outerCount = 0;
 let innerCount = 0;
-let loopCounts;
 let count5 = 0;
 
-while (randomNumber != 5 || count5 != 3) {
-  iterator++;
+do {
   outerCount++;
   randomNumber = rand(5, 10);
   console.log(randomNumber);
 
-  if (randomNumber == 5) {
+  for (let i = 0; i < randomNumber; i++) {
+    innerCount++;
+    randomInner = rand(5, 10);
+    console.log('Inner loop number:', randomInner);
+  }
+  if (randomNumber === 5) {
     count5++;
     console.log('The [', count5, '] cycle of-5 is over.');
   }
 
-  while (iterator <= randomNumber) {
-    iterator++;
-    innerCount++;
-    // randomNumber = rand(5, 10);
-    console.log('Inner loop gnrNumbers:', randomNumber);
-  }
-  loopCounts = outerCount + innerCount;
+} while (randomNumber !== 5 || count5 < 3);
 
-}
-
-console.log(`Isorinis ciklas kartojosi ${outerCount} kart. Vidinis ciklas kartojosi ${innerCount} kart.`)
-console.log(`Abu ciklai bendrai padare ${loopCounts} iterac.`);
+console.log(`Isorinis ciklas kartojosi ${outerCount} kart. Vidinis ciklas bendrai kartojosi ${innerCount} kart.`);
 
 console.log('10 ---------------------------');
 
-let taskaiPetro;
-let taskaiKazio;
-let rezPetro = 0;
-let rezKazio = 0;
-let laimetojas;
-while (rezPetro < 222 && rezKazio < 222) {
-  taskaiPetro = rand(10, 20);
-  taskaiKazio = rand(5, 25);
-  rezPetro += taskaiPetro;
-  rezKazio += taskaiKazio;
-  if (rezPetro >= 222) {
-    laimetojas = 'Petras';
-  } else {
-    laimetojas = 'Kazys';
-  }
-  console.log(`Petras: ${taskaiPetro} / Kazys: ${taskaiKazio}`);
-}
+// let taskaiPetro;
+// let taskaiKazio;
+// let rezPetro = 0;
+// let rezKazio = 0;
+// let laimetojas;
+// while (rezPetro < 222 && rezKazio < 222) {
+//   taskaiPetro = rand(10, 20);
+//   taskaiKazio = rand(5, 25);
+//   rezPetro += taskaiPetro;
+//   rezKazio += taskaiKazio;
+//   if (rezPetro >= 222) {
+//     laimetojas = 'Petras';
+//   } else {
+//     laimetojas = 'Kazys';
+//   }
+//   console.log(`Petras: ${taskaiPetro} / Kazys: ${taskaiKazio}`);
+// }
 
-console.log(`Petro rez.: ${rezPetro} / Kazio rez.: ${rezKazio}`)
-console.log(`Partija laimejo ${laimetojas}.`)
+// console.log(`Petro rez.: ${rezPetro} / Kazio rez.: ${rezKazio}`)
+// console.log(`Partija laimejo ${laimetojas}.`)
 
 
